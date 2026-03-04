d1beb821104e105ba13451e638d7bcfa1897cbb4
Bummanik <jens9100@hotmail.com>
2026-03-04 09:54:54 +0100

    -.-

--- ordination/Ordination.java
+++ ordination/Ordination.java
@@ -8,6 +8,8 @@ public abstract class Ordination {
     private LocalDate slutDen;
 
     // TODO Link til Laegemiddel
+    private Laegemiddel laegemiddel;
+
     // TODO constructor (med specifikation)
 
     public LocalDate getStartDen() {
